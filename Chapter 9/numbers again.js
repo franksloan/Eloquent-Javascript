614c9573319714097b628eca2fd6c50332752ab3
Frank Sloan <francis.m.sloan@gmail.com>
2015-03-04 18:42:11 +0000

    Different versions of js accepted numbers regexp test

---
+++ Chapter 9/numbers again.js
@@ -0,0 +1,18 @@
+//This is actually not strictly correct as it accepts '+' followed by '.'
+var number = /^(\+|-)?(\d+(\.\d*)?|\.\d+)(e(\+|-)?\d+)?$/i;
+// Tests:
+["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
+ "1e+12"].forEach(function(s) {
+  if (!number.test(s))
+    console.log("Failed to match '" + s + "'");
+});
+["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5",
+ "."].forEach(function(s) {
+  if (number.test(s))
+    console.log("Incorrectly accepted '" + s + "'");
+});
+//This version means that there has to be a number before the '.',
+// note the re-arrange of brackets.
+var number = /^((\+|-)?\d+(\.\d*)?|\.\d+)(e(\+|-)?\d+)?$/i;
+// Tests: above but with the second array containing +.1
+
